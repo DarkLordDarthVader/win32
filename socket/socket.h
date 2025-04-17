6a5955343e853e0bf7fe8591738a2af639407939
DarkLordDarthVader <kadzicuh@outlook.com>
2025-04-17 10:56:41 +0500

    Add files via upload

---
+++ socket/socket.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment (lib, "ws2_32.lib")
+
+namespace win32 {
+    class socket {
+    public:
+        socket(LPCWSTR strHost, LPCWSTR strPort) {
+            WSADATA wsaData;
+            int err = WSAStartup(
+                MAKEWORD(2, 2), 
+                &wsaData
+            );
+            ADDRINFOW hints;
+            ZeroMemory(
+                &hints, 
+                sizeof(hints)
+            );
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_STREAM;
+            hints.ai_protocol = IPPROTO_TCP;
+            err = GetAddrInfoW(
+                strHost, 
+                strPort, 
+                &hints, 
+                &this->paiw
+            );
+        };
+        ~socket() {
+            FreeAddrInfoW(this->paiw);
+            WSACleanup();
+        };
+        void open() {
+            this->sock = ::socket(
+                this->paiw->ai_family, 
+                this->paiw->ai_socktype, 
+                this->paiw->ai_protocol
+            );
+        }
+        void close() {
+            closesocket(this->sock);
+            this->sock = INVALID_SOCKET;
+        }
+        int connect() {
+            return ::connect(
+                this->sock, 
+                this->paiw->ai_addr, 
+                (int)this->paiw->ai_addrlen
+            );
+        }
+    private:
+        PADDRINFOW paiw;
+        SOCKET sock;
+    };
+}
